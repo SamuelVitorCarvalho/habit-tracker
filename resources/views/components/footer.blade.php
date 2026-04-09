1987bcfbac5a357c1ee10cb6a2c6731387683a55
Samuel Vitor Gomes de Carvalho <samuelvitor124@gmail.com>
2026-04-09 20:10:38 -0300

    feat: init components header/footer/layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t border-t-2 p-4">
+    <p class="text-center text-gray-600">
+        Criado por Samuel Carvalho. O código fonte está no <a href="#" class="hover:underline">GitHub</a>.
+    </p>
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,13 @@
-<!doctype html>
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>
-            {{ config('app.name') }}
-        </title>
+<x-layout>
 
-        @vite('resources/css/app.css')
-    </head>
-    
-    <body>
+    <main class="py-10">
+
         <div class="flex items-center justify-center h-screen">
             <h1 class="text-4xl font-bold text-gray-800">
                 Bem-vindo ao {{ config('app.name') }}!
             </h1>
         </div>
-    </body>
-</html>
+
+    </main>
+
+</x-layout>
