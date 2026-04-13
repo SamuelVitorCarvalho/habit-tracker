0df60b95a406510d7b125cf7c7eb80c293db07ae
Samuel Vitor Gomes de Carvalho <samuelvitor124@gmail.com>
2026-04-13 19:10:12 -0300

    feat(Habit): create toggle functionality

--- app/Http/Controllers/HabitController.php
+++ app/Http/Controllers/HabitController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Habit;
+use App\Models\HabitLog;
+use Carbon\Carbon;
 use Illuminate\View\View;
 use App\Http\Requests\HabitRequest;
 use Illuminate\Support\Facades\Auth;
@@ -84,5 +86,41 @@ class HabitController extends Controller
         $habits = Auth::user()->habits;
 
         return view('habits.settings', compact('habits'));
+    }
+
+    public function toggle(Habit $habit)
+    {
+        // 1.Verificar se o usuário autenticado é o dono do hábito
+        if($habit->user_id !== Auth::user()->id){
+            abort(403, 'Esse hábito não é seu!!!');
+        }
+
+        // 2.Pegar a data de hoje
+        $today = Carbon::today()->toDateString();
+
+        // 2.1 Pegar o log
+        $log = HabitLog::query()
+            ->where('habit_id', $habit->id)
+            ->where('completed_at', $today)
+            ->first();
+
+        // 3.Validar se nessa data já existe um registro
+        if($log){
+            // 4.Se existir, remover o registro
+            $log->delete();
+            $message = 'Hábito desmarcado.';
+        } else {
+            // 5.Se não existir, criar o registro
+            HabitLog::create([
+                'user_id' => Auth::user()->id,
+                'habit_id' => $habit->id,
+                'completed_at' => $today,
+            ]);
+            $message = 'Hábito concluído 👏';
+        }
+        // 6.Retornar para a página anterior
+        return redirect()
+            ->route('habits.index')
+            ->with('success', $message);
     }
 }
