4accccf7f9d01217275981b32822ffb75f387f18
Samuel Vitor Gomes de Carvalho <samuelvitor124@gmail.com>
2026-04-13 19:54:54 -0300

    feat: create contribution grid

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,37 +1,36 @@
-<header class="bg-white border-bottom border-b-2 flex items-center justify-between p-4">
+<header class="bg-white border-b-2">
     
-    {{-- LOGO --}}
-    <div class="flex items-center gap-2 font-bold">
-        <a href="{{ route('habits.index') }}" class="habit-btn habit-shadow-lg px-2 py-1 bg-habit-orange">
-            HT
-        </a>
-        <p>
-            Habit Tracker
-        </p>
-    </div>
-
-    {{-- GITHUB --}}
-    <div>
-        @auth
-        <form class="inline" action="{{ route('auth.logout') }}" method="POST">
-            @csrf
-
-            <button type="submit" class="habit-shadow-lg p-2 habit-btn">
-                Sair
-            </button>
-        </form>
-        @endauth
-
-        @guest
-            <div class="flex gap-2">
-                <a href="{{ route('site.register') }}" class="habit-shadow-lg p-2 habit-btn">
-                    Cadastrar
-                </a>
-                <a href="{{ route('site.login') }}" class="bg-habit-orange habit-shadow-lg p-2 habit-btn">
-                    Login
-                </a>
-            </div>
-        @endguest
+    <div class="max-w-7xl mx-auto flex items-center justify-between p-4">
+        {{-- LOGO --}}
+        <div class="flex items-center gap-2 font-bold">
+            <a href="{{ route('habits.index') }}" class="habit-btn habit-shadow-lg px-2 py-1 bg-habit-orange">
+                HT
+            </a>
+            <p>
+                Habit Tracker
+            </p>
+        </div>
+        {{-- GITHUB --}}
+        <div>
+            @auth
+            <form class="inline" action="{{ route('auth.logout') }}" method="POST">
+                @csrf
+                <button type="submit" class="habit-shadow-lg p-2 habit-btn">
+                    Sair
+                </button>
+            </form>
+            @endauth
+            @guest
+                <div class="flex gap-2">
+                    <a href="{{ route('site.register') }}" class="habit-shadow-lg p-2 habit-btn">
+                        Cadastrar
+                    </a>
+                    <a href="{{ route('site.login') }}" class="bg-habit-orange habit-shadow-lg p-2 habit-btn">
+                        Login
+                    </a>
+                </div>
+            @endguest
+        </div>
     </div>
 
 </header>
