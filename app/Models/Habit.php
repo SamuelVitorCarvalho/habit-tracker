6980c3298d404212586bbfae723ff19165a12f0d
Samuel Vitor Gomes de Carvalho <samuelvitor124@gmail.com>
2026-04-13 19:29:48 -0300

    feat(Habit): add wasCompletedToday method

--- app/Models/Habit.php
+++ app/Models/Habit.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class Habit extends Model
 {
@@ -16,15 +18,24 @@ class Habit extends Model
             'name',
         ];
 
-        // Um hábito pertence a um usuário
-        public function user(): BelongsTo
-        {
-            return $this->belongsTo(User::class);
-        }
+    // Um hábito pertence a um usuário
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
-        // Um hábito pode ter muitos registros
-        public function habitLogs(): HasMany
-        {
-            return $this->hasMany(HabitLog::class);
-        }
+    // Um hábito pode ter muitos registros
+    public function habitLogs(): HasMany
+    {
+        return $this->hasMany(HabitLog::class);
+    }
+
+    public function wasCompletedToday(): bool
+    {
+        $today = Carbon::today()->toDateString();
+
+        return $this->habitLogs()
+            ->whereDate('completed_at', $today)
+            ->isNotEmpty();
+    }
 }
